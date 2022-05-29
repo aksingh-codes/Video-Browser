de319e652a2f763f529d22065b6e10b420a9260d
aksingh <dev.ak.singh@outlook.com>
2022-05-29 18:01:32 +0530

    update responsiveness

--- src/components/App.js
+++ src/components/App.js
@@ -32,7 +32,7 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <div className="ui grid">
+        <div className="ui stackable grid">
           <div className="ui row">
             <div className="eleven wide column">
               <VideoDetail video={this.state.selectedVideo} />
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,10 +1,14 @@
-import './VideoItem.css';
-import React from 'react';
+import "./VideoItem.css";
+import React from "react";
 
 const VideoItem = ({ video, onVideoSelect }) => {
   return (
     <div onClick={() => onVideoSelect(video)} className="video-item item">
-      <img alt={video.snippet.title} className="ui image" src={video.snippet.thumbnails.medium.url} />
+      <img
+        alt={video.snippet.title}
+        className="ui image"
+        src={video.snippet.thumbnails.medium.url}
+      />
       <div className="content">
         <div className="header">{video.snippet.title}</div>
       </div>
