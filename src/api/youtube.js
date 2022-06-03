47a405d018267935cf29469baddb9a78827702b3
aksingh <dev.ak.singh@outlook.com>
2022-06-03 15:16:39 +0530

    added api

---
+++ src/api/youtube.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const KEY = process.env.REACT_APP_YOUTUBE_KEY;
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    type: "video",
+    maxResults: 10,
+    key: KEY,
+  },
+});
